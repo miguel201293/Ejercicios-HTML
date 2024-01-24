4487d448e3c2b35ab931505144093c5188a03ed5
miguel201293 <miguel.abato.93@gmail.com>
2024-01-24 11:29:34 +0100

    cambios

--- javascriptPruebas.js
+++ javascriptPruebas.js
@@ -17,10 +17,10 @@ let estudiantes = [
     city:"Malaga",
     hobby:"Futbol",
     favoriteFood:"Arroz",
-    favoriteVideoGame:"Lol",
+    favoriteVideoGame:"LOL",
     favoriteFilm:"Grease",
     favoriteBook:"50 sombras",
-    petName: "Mr.Rabbit"
+    petName: null
     },
     {name:"Rosa",
     surname:"Martinez",
@@ -28,10 +28,10 @@ let estudiantes = [
     city:"Almeria",
     hobby:"Lectura",
     favoriteFood:"Verduras",
-    favoriteVideoGame:"Animal crossing",
+    favoriteVideoGame:"League Of Legends",
     favoriteFilm:"mis primeras 50 citas",
     favoriteBook:"No lo llamaes amor",
-    petName: "Minnie"
+    petName: null
     },
     {name:"Paco",
     surname:"Porras",
@@ -39,23 +39,94 @@ let estudiantes = [
     city:"Huelva",
     hobby:"Tenis",
     favoriteFood:"Pescado",
-    favoriteVideoGame:"Lol",
+    favoriteVideoGame:"Assasin Creed",
     favoriteFilm:"Mr.Robot",
     favoriteBook:"Comic DC",
-    petName: "kiara"
+    petName: "Kiara"
     }
 ];
 // Mostramos el array por pantalla
-//console.log(estudiantes);
-// Mostrar nombres similares
-function similares (array){
-    let duplicados =[]
-    for ( let i = 0; i < array.length; i ++){
-        let elemento = array[i].name
-        if (!Duplicados == elemento && elemento)
+// funcion para ordenar alfabeticamente por apellidos los objetos del array.
+function OrdenAlfabetico (array){
+    estudiantes.sort(function(a, b) { // sort ordena los elementos de array
+        let surnameA = a.surname.toUpperCase(); // crea variable surname y 
+        let surnameB = b.surname.toUpperCase(); //toUpperCase lo hace mayuscula
+    
+        if (surnameA < surnameB) {             
+            return -1;
+        } else if (surnameA > surnameB) {   
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+    
+    estudiantes.forEach(function(estudiante) {
+        console.log(estudiante);
+    });
+}
+// Funcion para ordenar por edad a los estudiantes.
+function OrdenarEdad (array){
+    estudiantes.sort(function(a, b){
+        return a.age - b.age;
+    });
+    
+    estudiantes.forEach(function(estudiante){
+        console.log(estudiante.name,estudiante.age);
+    });
+}
+// Funcion para mostrar la edad media de los estudiantes.
+function MediaEdad (array){
+    let sumaEdades = 0;
+
+    for(let i = 0; i < estudiantes.length; i++){
+        sumaEdades = sumaEdades + estudiantes[i].age;
     }
 
-    return duplicados
+    let edadMedia = sumaEdades / estudiantes.length;
+
+    console.log("La edad media de los estudiantes es: ",edadMedia);
 }
-console.log(similares(estudiantes))
+// Funcion para mostrar los estudiantes que juegan al LOL
+function JuegaLOL (array){
+    let juegan = []
+    for (i = 0; i < array.length; i++){
+        if (array[i].favoriteVideoGame == "LOL" || array[i].favoriteVideoGame == "League Of Legends"){
+            juegan.push(array[i].name)
+        }
+    }
+    console.log("Los estudiantes que juegan al LOL son: ",juegan)
+}
+// Funcion para mostrar el nombre repetido en el array.
+function Repetidos (array){
+    let nombres = []
+    for(i = 0; i < array.length; i ++){
+        nombres.push(array[i].name)
+    }
+    let duplicados = []
+    for (i = 0; i < nombres.length; i ++){
+        let nombre = nombres[i]
+        if(!duplicados.find(n => n == nombre) && nombres.slice(i + 1).find(n => n == nombre)){
+            duplicados.push(nombre)
+        }
+    }
+    console.log("Los nombres que se repiten son: ",duplicados)
+}
+// Funcion para mostrar los nombres de quien tiene mascotas.
+function mascotas (array){
+    let nombre = []
+    for (i = 0; i < array.length; i++){
+        if (array[i].petName != null) {
+            nombre.push(array[i].name)
+        }
+    }
+    console.log("Los estudiantes que tienen mascotas son: ",nombre)
+}
+// invocamos las funciones
+OrdenAlfabetico(estudiantes);
+OrdenarEdad(estudiantes);
+MediaEdad(estudiantes);
+mascotas(estudiantes);
+JuegaLOL(estudiantes);
+Repetidos(estudiantes);
 
